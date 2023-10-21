40d1bd0edf5f97e7cabd90b536df84cc1c3fe2c4
22091397033 Aryangga Putra Satrya <117990980+22091397033AryanggaPutraSatrya@users.noreply.github.com>
2023-10-21 11:01:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,22 @@
                 backToTop.style.display = "none";
             }
         });
+
+
+var openButtons = document.querySelectorAll(".openVideo");
+var videoPopup = document.getElementById("videoPopup");
+var videoIframe = document.getElementById("videoIframe");
+var closeVideo = document.getElementById("closeVideo");
+        
+openButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        var youtubeURL = this.getAttribute("data-video");
+        videoIframe.src = youtubeURL;
+        videoPopup.style.display = "block";
+    });
+});
+
+closeVideo.addEventListener("click", function() {
+    videoPopup.style.display = "none";
+    videoIframe.src = "";
+});
