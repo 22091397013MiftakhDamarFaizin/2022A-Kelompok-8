875a47cbbc9e28e5678383d23d5b21750ef19689
22091397013 Miftakh Damar Faizin <144097482+22091397013MiftakhDamarFaizin@users.noreply.github.com>
2023-10-21 14:36:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 window.addEventListener("scroll", function () {
-        var backToTop = document.getElementById("back-to-top");
-        var containernavbar = document.querySelector(".containernavbar");
+        let backToTop = document.getElementById("back-to-top");
+        let containernavbar = document.querySelector(".containernavbar");
         if (window.scrollY > containernavbar.clientHeight) {
             backToTop.style.display = "inline";
         } else {
             backToTop.style.display = "none";
         }
-    });
+});
 
 
-var openButtons = document.querySelectorAll(".openVideo");
-var videoPopup = document.getElementById("videoPopup");
-var videoIframe = document.getElementById("videoIframe");
-var closeVideo = document.getElementById("closeVideo");
-        
+let openButtons = document.querySelectorAll(".openVideo");
+let videoPopup = document.getElementById("videoPopup");
+let videoIframe = document.getElementById("videoIframe");
+let closeVideo = document.getElementById("closeVideo");
+
 openButtons.forEach(function(button) {
     button.addEventListener("click", function() {
-        var youtubeURL = this.getAttribute("data-video");
+        let youtubeURL = this.getAttribute("data-video");
         videoIframe.src = youtubeURL;
         videoPopup.style.display = "block";
     });
 });
 
 closeVideo.addEventListener("click", function() {
+    // Menghentikan video ketika pop-up ditutup
+    videoIframe.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
     videoPopup.style.display = "none";
 });
 
 videoPopup.addEventListener("click", function(event) {
-        if (event.target === this) {
+    if (event.target === this) {
+        // Menghentikan video ketika area luar iframe diklik
+        videoIframe.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
         videoPopup.style.display = "none";
-        }
+    }
 });
